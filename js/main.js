dc675dbd328542018fd3db000f8a5452197f4299
Ivan Dubynets <ivan.dubynets@gmail.com>
2023-04-26 10:48:12 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@
 
 $(function() {
     initFormValidation();
-    initInputPinControl(".input-pin-control");
 });
 
 function initFormValidation() {
@@ -57,74 +56,5 @@ function genericFormsValidation(event, form) {
     }
 
     form.classList.add('was-validated')
-}
-
-
-function initInputPinControl(selector) {
-    const inputPinControls = document.querySelectorAll(selector);
-    inputPinControls.forEach(pinControl => {
-        const inputs = pinControl.querySelectorAll('.pin-input');
-        let focusedIndex = -1;
-
-        inputs.forEach((input, key) => {
-            input.addEventListener("paste", (event) => {
-                event.preventDefault();
-            });
-
-            input.addEventListener("click", function() {
-                let inputFocused = false;
-                for (let key = 0; key < inputs.length; key++) {
-                    let input = inputs[key];
-                    if (!input.value) {
-                        inputs[key].focus();
-                        focusedIndex = key;
-                        inputFocused = true;
-                        break;
-                    }
-                }
-                if (!inputFocused) {
-                    inputs[inputs.length - 1].focus();
-                    focusedIndex = inputs.length - 1;
-                }
-            });
-            input.addEventListener("keydown", function(event) {
-                if (event.keyCode == 8) {
-                    if (!input.value && key > 0) {
-                        inputs[key - 1].focus();
-                        focusedIndex = key - 1;
-                    }
-                }
-            })
-            input.addEventListener("keyup", function() {
-                if (input.value) {
-                    if (key + 1 < inputs.length) {
-                        inputs[key + 1].focus();
-                        focusedIndex = key + 1;
-                    }
-                }
-            });
-        });
-
-        pinControl.addEventListener("paste", (event) => {
-            let paste = (event.clipboardData || window.clipboardData).getData("text");
-            if (paste.length == 0) return;
-
-            if (focusedIndex == -1) {
-                focusedIndex = 0;
-            }
-
-            let i = focusedIndex;
-            while (i < inputs.length && i < paste.length) {
-                inputs[i].value = paste[i];
-                i++;
-            }
-
-            if (i < inputs.length) {
-                inputs[i].focus();
-            } else {
-                inputs[inputs.length - 1].focus();
-            }
-        });
-    })
 }
 //# sourceMappingURL=main.js.map
